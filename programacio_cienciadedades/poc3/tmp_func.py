f70f55af43ae927ebbe80911b7145843a8715930
alvimn <albertlozanovalverde@gmail.com>
2025-01-09 07:58:25 +0100

    pk4

---
+++ programacio_cienciadedades/poc3/tmp_func.py
@@ -0,0 +1,6 @@
+def task(n: int):
+    """Devuelve el factorial de un número:
+    Los datos de entrada será el número a calcular,
+    mientras que los datos que devuelve es el factorial del número ya calculado
+    """
+    return math.factorial(n)
