3616afa9b3d3495854cd98803ec8704019e7ce2c
alvimn <118203570+alvimn@users.noreply.github.com>
2025-01-17 11:06:47 +0100

    Update setup.py

--- programacio_cienciadedades/poc4/activity_4-master/setup.py
+++ programacio_cienciadedades/poc4/activity_4-master/setup.py
@@ -9,10 +9,25 @@ import pandas as pd
 
 
 def datapull():
+     """
+    Loads the dataset from a CSV file, prints basic details of the dataset,
+    and returns the DataFrame.
+
+    Returns:
+    DataFrame: Loaded dataset as a pandas DataFrame.
+    """
+    # Load the dataset from the specified CSV file
     df = pd.read_csv("data/dataset.csv", sep=";")
+
+    # Print the first 5 entries of the dataset
     print("Les 5 primeres entrades del dataset son: \n")
     print(df.head(5))
+
+    # Print the total number of entries in the dataset
     print("\n El data set te " + str(len(df)) + " entrades \n")
+    
+    # Print the column names of the dataset
     print("Les columnes del data set son:\n")
     print(list(df.columns))
+
     return (df)
