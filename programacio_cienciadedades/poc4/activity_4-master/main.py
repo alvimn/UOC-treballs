32c51c11f62b0fe56034a6e476ba42c62d3c0187
alvimn <118203570+alvimn@users.noreply.github.com>
2025-01-17 11:03:24 +0100

    Update main.py

--- programacio_cienciadedades/poc4/activity_4-master/main.py
+++ programacio_cienciadedades/poc4/activity_4-master/main.py
@@ -6,15 +6,24 @@ Created on Thu Jan 16 07:23:00 2025
 @author: albert
 """
 
+# Import necessary modules
 import setup
 import anomize
 import minutagg
 import fix_club
 import team_analisis
 
+# Load the dataset using the datapull function from setup module
 df = setup.datapull()
-df = anomize.name_surname(df)
-df = minutagg.gruptimes(df)
-df = fix_club.club_depuration(df)
-team_analisis.analisis(df, "UCSC")
 
+# Anonymize the 'biker' names in the dataset
+df = anomize.name_surname(df)
+
+# Group times in the dataset into intervals and plot a histogram
+df = minutagg.gruptimes(df)
+
+# Clean and depurate club names in the dataset
+df = fix_club.club_depuration(df)
+
+# Analyze the team data for the "UCSC" team
+team_analisis.analisis(df, "UCSC")
