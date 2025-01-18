cb647237ae7988abb442de05d2f820d1fb5ce4a8
alvimn <118203570+alvimn@users.noreply.github.com>
2025-01-18 18:56:50 +0100

    Update setup.py

--- programacio_cienciadedades/poc4/activity_4-master/setup.py
+++ programacio_cienciadedades/poc4/activity_4-master/setup.py
@@ -5,29 +5,24 @@ Created on Thu Jan 16 07:24:05 2025
 
 @author: albert
 """
-import pandas as pd
+from setuptools import setup, find_packages
 
-
-def datapull():
-    """
-   Loads the dataset from a CSV file, prints basic details of the dataset,
-   and returns the DataFrame.
-
-   Returns:
-   DataFrame: Loaded dataset as a pandas DataFrame.
-   """
-    # Load the dataset from the specified CSV file
-    df = pd.read_csv("data/dataset.csv", sep=";")
-
-    # Print the first 5 entries of the dataset
-    print("Les 5 primeres entrades del dataset son: \n")
-    print(df.head(5))
-
-    # Print the total number of entries in the dataset
-    print("\n El data set te " + str(len(df)) + " entrades \n")
-
-    # Print the column names of the dataset
-    print("Les columnes del data set son:\n")
-    print(list(df.columns))
-
-    return (df)
+setup(
+    name="POC4",
+    version="0.1.0",
+    author="Albert",
+    description="Data processing and analysis scripts for POC4",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "faker",
+        "matplotlib",
+    ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
