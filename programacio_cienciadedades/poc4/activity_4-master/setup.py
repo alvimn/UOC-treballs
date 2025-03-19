76f2567be85f682d0c50ad5dc5c0268594bbae44
alvimn <albertlozanovalverde@gmail.com>
2025-03-19 12:10:55 +0100

    all

--- programacio_cienciadedades/poc4/activity_4-master/setup.py
+++ programacio_cienciadedades/poc4/activity_4-master/setup.py
@@ -18,7 +18,6 @@ setup(
         "faker",
         "matplotlib",
     ],
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
